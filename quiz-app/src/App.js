cc3f7f89b6c8d42b56a439d6cc4d4f17652f24a9
prwt <perawit37@gmail.com>
2023-09-15 12:26:12 +0700

    Initial Commit (setup on window)

---
+++ quiz-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Web development Quiz</h1>
+      <h4>(Class Homework)</h4>
+    </div>
+  );
+}
+
+export default App;
